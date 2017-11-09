055ceda83a67e11a8e04c9e53d293588d263f97d
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2017-11-09 18:58:03 +0100

    Ajout d'un README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# CMD Line
+* Premier terminal
+    * `npm install`
+    * `npm start`
+* Second terminal
+    * `mongod --dbpath ./data` || `mongod`
+* Troisième terminal
+    * `mongo`
+    * Si aucune collections :
+        * `use tasks`
+        * `db.list.insert([{title:"Installer MongoDB", isDone: false}, {title: "Configurer MongoDB", isDone: false}, {title:"Utiliser le shell MongoDB", isDone: false})]`
+
+# Commandes utiles pour MongoDB
+* `show dbs`
+* `db.list.find({isDone : false}).pretty()`
--- client/js/app.js
+++ client/js/app.js
@@ -1,31 +1,34 @@
-const tasksUrl = 'http://127.0.0.1:8080/api/tasks';
+const tasksUrl = "/api/tasks";
 
 fetch(tasksUrl, {
-    method: 'get'
-}).then((response, err) => {
+  method: "get"
+})
+  .then((response, err) => {
     if (response.status === 200) {
-        return response.json();
+      return response.json();
     } else {
-        throw `Erreur avec la requête API : ${response.statusText}`;
+      throw `Erreur avec la requête API : ${response.statusText}`;
     }
-}).then(arrayOfTasks => {
-        console.log(arrayOfTasks);
-        arrayOfTasks.map(elem => {
-            componentTask(elem);
-        })
-    }
-).catch(err => {
+  })
+  .then(arrayOfTasks => {
+    console.log(arrayOfTasks);
+    arrayOfTasks.map(elem => {
+      componentTask(elem);
+    });
+  })
+  .catch(err => {
     console.error(err);
-});
+  });
 
-const componentTask = (elem) => {
-    const tasksList = document.querySelector('.tasksList');
-    
-    const task = document.createElement('div').innerHTML =
-    `<article data-id="${elem._id}" class="taskListItem taskIsDone${elem.isDone}">
+const componentTask = elem => {
+  const tasksList = document.querySelector(".tasksList");
+
+  const task = (document.createElement(
+    "div"
+  ).innerHTML = `<article data-id="${elem._id}" class="taskListItem" data-task-is-done=${elem.isDone}>
         <h2>${elem.title}</h2>
-    </article>`;
+    </article>`);
 
-    tasksList.innerHTML += task;
-    console.log(e);
-}
+  tasksList.innerHTML += task;
+  console.log(elem);
+};
