13bf39874cbfb72d46520360e2012cc5acd7d0fa
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2017-11-10 17:02:56 +0100

    13. Utilisation du service Angular

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -11,8 +11,62 @@ import { MongodbService } from "../../services/mongodb.service";
 export class HomeComponent implements OnInit {
   constructor(
     // Ajouter le service dans le constructor
-    private mongoddbService: MongodbService
+    private mongodbService: MongodbService
   ) {}
+
+  // Fonction showTasks()
+  private tasksCollection: any[];
+
+  private showTasks = () => {
+    // Appel de la fonction du service getAllTasks()
+    this.mongodbService.getAllTasks().then(data => {
+      // Callback => Injecter les données dans un tableau
+      this.tasksCollection = data;
+    });
+  };
+
+  // Fonction addNewTask()
+  // La nouvelle tâche est passée en paramètre de la fonction.
+  private addNewTask = (taskTitle: any) => {
+    // Définition de l'objet newTask
+    let newTask = {
+      title: taskTitle,
+      isDone: false
+    };
+
+    // Appel de la fonction du service addNewTask()
+    this.mongodbService.addNewTask(newTask).then(mongoNewTask => {
+      // Callback => Actualiser la liste des tâches
+      this.showTasks();
+    });
+  };
+
+  // Fonction updateTask()
+  // La tâche à modifier est passée en paramètre de la fonction
+  private updateTask = task => {
+    // Définition d'une variable pour mettre à jour les données de la tâche
+    let _task = {
+      _id: task.id,
+      title: task.title,
+      isDone: !task.isDone
+    };
+
+    // Appel de la fonction du service updateTask()
+    this.mongodbService.updateTask(_task).then(data => {
+      // Callback => Actualiser la liste des tâches
+      this.showTasks();
+    });
+  };
+
+  // Fonction deleteTask()
+  // L'id de la tâche à supprimer est passée en paramètre de la fonction
+  private deleteTask = id => {
+    // Appel de la fonction du service deleteTask()
+    this.mongodbService.deleteTask(id).then(data => {
+      // Callback => Actualiser la liste des tâches
+      this.showTasks();
+    });
+  };
 
   ngOnInit() {}
 }
--- client/src/app/services/mongodb.service.ts
+++ client/src/app/services/mongodb.service.ts
@@ -17,17 +17,17 @@ export class MongodbService {
   private editTaskUrl = `${apiUrl}/api/task`;
 
   // Requête GET
-  private getAllTasks(): Promise<any[]> {
+  getAllTasks = (): Promise<any[]> => {
     // Récupérer les données depuis la BDD MongoDb
     return this.http
       .get(this.getTasksUrl)
       .toPromise()
       .then(this.dataFromMongodb)
       .catch(this.handleError);
-  }
+  };
 
   // Requ$ete POST
-  private addNewTask(newTask): Promise<any[]> {
+  addNewTask = (newTask): Promise<any[]> => {
     // Définition du header de la requête
     let myHeader = new Headers();
     myHeader.append("Content-Type", "application/json");
@@ -38,10 +38,10 @@ export class MongodbService {
       .toPromise()
       .then(this.dataFromMongodb)
       .catch(this.handleError);
-  }
+  };
 
   // Requête PUT
-  private updateTask(task): Promise<any[]> {
+  updateTask = (task): Promise<any[]> => {
     // Définition du header de la requête
     let headers = new Headers();
     headers.append("Content-type", "application/json");
@@ -52,28 +52,28 @@ export class MongodbService {
       .toPromise()
       .then(this.dataFromMongodb)
       .catch(this.handleError);
-  }
+  };
 
   // Requête DELETE
-  private deleteTask(id): Promise<any[]> {
+  deleteTask = (id): Promise<any[]> => {
     // Suppresion de la tâche de la BDD MongoDb
     return this.http
       .delete(`${this.editTaskUrl}/${id}`)
       .toPromise()
       .then(this.dataFromMongodb)
       .catch(this.handleError);
-  }
+  };
 
   // Return data
-  private dataFromMongodb(res: Response) {
+  private dataFromMongodb = (res: Response) => {
     return res.json() || {};
-  }
+  };
 
   // Return error
-  private handleError(error: any) {
+  private handleError = (error: any) => {
     let errMsg = error.message
       ? error.message
       : error.status ? `${error.status} - ${error.statusText}` : "Server error";
     return Promise.reject(errMsg);
-  }
+  };
 }
